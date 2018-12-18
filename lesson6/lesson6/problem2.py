beea1991ea37ec7b1a26061cd52e2ca077c7bb31
vallettafox <43784504+vallettafox@users.noreply.github.com>
2018-12-17 23:48:15 -0500

    Add files via upload

---
+++ lesson6/lesson6/problem2.py
@@ -0,0 +1,15 @@
+name = input('What is your name? ')
+
+print('Cool name, ' + name + '!')
+
+age = input('Weird question, but how old are you? ')
+
+print("You're " + age.lower() + "?! " + "That's kinda old, " + name + ".")
+
+feeling = input("So " + name + ", how are you feeling today? ")
+
+print(feeling.upper() + " huh, that's good to hear.")
+
+movie = input("What's your favorite movie? ")
+
+print("Oh, I heard of" + movie + "." + " I want to watch that one eventually.")
---
+++ lesson6/lesson6/problem3.py
@@ -0,0 +1,17 @@
+name = input("So loser, what's your name? ")
+
+print(name.upper() + "?" + "Wow, that's a disgusting name. Change it immediataly.")
+
+color1 = input("Does a loser like you even have a favorite color? Bet it's something basic like black. ")
+
+color2 = input(color1.capitalize() + "? " + "Well, what is it? ")
+
+print("You know that " + color2.lower() + " is totally two years ago. Have you beem living under a rock or something?")
+
+subject = input("What is your favorite subject in school, " + name + "?" + "If you're even smart enough to pass your classes.")
+
+print(subject.upper() + " IS LITERALLY THE WORST CLASS WHAT THE %$@#!!! " + " You're such a nerd.")
+
+annoying = input("You're starting to annoy me. Can you please just go away now? ")
+
+print("Well I don't care. I hate you. Bye.")
